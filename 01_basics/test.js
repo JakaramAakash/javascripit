7096dd2f9b748311c350012fa625a93b7116ef83
JakaramAakash <124370735+JakaramAakash@users.noreply.github.com>
2024-04-03 12:49:26 +0000

    commit

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,22 @@
+console.time()
 console.log("Aakash shiva")
+const accountId=4567
+let email="jakaramaakash@gmail.com"
+let passward="12345"
+accountArea=3563
+console.table([accountId,email,passward,accountArea]);
+const product=
+       {
+        name:"Aakash",
+        roll:123
+       }
+       const product2={
+        name:"shiva",
+        roll:1
+       }
+       console.table([product,product2]);
+       console.warn("hello,warn 1")
+       console.error("error1")
+      
+       console.timeLog()
+       console.timeEnd();
