d64113bac48f3f6b44e04c01e890c12cdd604af8
JakaramAakash <124370735+JakaramAakash@users.noreply.github.com>
2024-04-05 04:10:28 +0000

    cmt

---
+++ 01_basics/nums.js
@@ -0,0 +1,26 @@
+/*const a=new Number(100)//object of type Number
+console.log(a)
+console.log(a.toString().length)
+console.log(a.toFixed(5))//precision value(use pv=2 in e-commerce websites)
+const b=23.456
+console.log(b.toFixed(2))//>=5(ceil)<5(floor)
+console.log(b.toPrecision(2))
+const c=10000000
+console.log(c.toLocaleString())
+console.log(c.toLocaleString('en-IN'))//Indian Standard
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.isFinite)
+console.log(Number.isNaN)*/
+/***************************************Maths*************************************** */
+/*console.log(Math)
+let a=22
+console.log(Math.round(4.5))
+console.log(Math.sqrt(a))
+console.log(a**a)
+console.log(Math.max(a,34))
+console.log(Math.floor(5.6));console.log(Math.ceil(4.6))
+console.log(Math.max(3,4,23,12,4))
+console.log(Math.min(45,34,2,4,2))*/
+console.log(Math.random())//values between 0 and 1
