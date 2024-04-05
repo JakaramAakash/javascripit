982de851060761af6f9bf654dd04a6fb3dd6e3c1
JakaramAakash <124370735+JakaramAakash@users.noreply.github.com>
2024-04-05 11:23:48 +0000

    nhmngvg

---
+++ 01_basics/arrays.js
@@ -0,0 +1,26 @@
+const arr=[1,2,3,1,3,4]
+const ar=["shiva","ravi","narayan"]
+ar.push(5)
+ar.pop()
+console.log(ar)
+console.log(ar)
+console.log(arr[0])
+arr.push(5)
+console.log(arr);arr.unshift(arr[0])
+console.log(arr)
+arr.shift()
+console.log(arr)
+arr.sort()
+console.log(arr)
+console.log(arr.includes(3));console.log(arr.indexOf(5));
+let ar1=ar.join();console.log(ar1)//ar1 is "String" type variable not array
+ar.splice(1,3)
+console.log("Arr",ar,"is Spliced")
+const bs=new String("Aakash")
+console.log(bs)
+bs.slice(1,3);console.log(bs)//does not change because Strings are immutable
+console.log(bs.slice(2,-1))
+let b1=new String("eeshwara");console.log(b1.slice(1,4))
+b2=b1.slice(1,7)//can be stored in another array
+console.log(b2)
+let b="Shiva";console.log(b.slice(1,4))
