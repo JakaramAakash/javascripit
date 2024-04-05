71d9c093077d2b323f04b0c4db044ee056cf197d
JakaramAakash <124370735+JakaramAakash@users.noreply.github.com>
2024-04-05 13:08:39 +0000

    dgdb

--- 01_basics/arrays2.js
+++ 01_basics/arrays2.js
@@ -21,4 +21,19 @@ console.log(t)*/
 const z=[2,3,54,[3,4,31],4,5,2,[4,5,3,5,[3,54,32,22],7,8]]
 console.log(z)
 console.log(z[7][4][2])
+//flat function
 const z1=z.flat(Infinity);console.log(z1)
+console.log(Array.isArray(z1))
+const a="Aakash"
+console.log(Array.isArray(a))
+b=a.split();console.log(b)
+t=Array.from(a);
+console.log(t)
+let score1=345
+let s2=56
+let s3=564
+let t1=Array.of(score1,s2,s3,a,z)
+console.log("Array t1=")
+console.log(t1)
+console.log("Array t=")
+console.log(Array.of(score1,s2,s3))
